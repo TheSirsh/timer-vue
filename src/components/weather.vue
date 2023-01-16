d32473297e59157a5718f35804636bb1a82566ee
TheSirsh <sirsh@bk.ru>
2023-01-16 20:23:19 +0300

    feat: add weather images

--- src/components/weather.vue
+++ src/components/weather.vue
@@ -1,7 +1,9 @@
 <template>
-  <div>
+  <div class="weather">
     <input type="text" class="city" />
-    <i class="weather-icon owf"></i>
+    <i class="weather-icon owf"
+      v-bind:class="iconClass"
+    ></i>
     <div class="weather-error"></div>
     <div class="description-container">
       <span class="temperature"> {{ weatherData.main.temp }} °C</span>
@@ -21,7 +23,7 @@
           weather: [{
             main: String,
             description: String,
-            icon: String,
+            id: String,
           }],
           main: {
             temp: Number,
@@ -33,6 +35,12 @@
         }
       }
     },
+    computed: {
+      iconClass: function() {
+        return "owf-" + this.weatherData.weather[0].id
+        
+      }
+    },
     mounted() {
       fetch('https://api.openweathermap.org/data/2.5/weather?q=Saint%20Petersburg&lang=en&appid=a930528912396a5ecf6b0001ff49f152&units=metric')
         .then(response => response.json())
@@ -42,3 +50,35 @@
     }
   }
 </script>
+
+<style scoped>
+  @import url("../assets/css/owfont-regular.css");
+
+  .weather {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: flex-start;
+    row-gap: calc(100vw / 204.8);
+    width: calc(100vw / 5.69);
+    min-height: 180px;  
+    text-align: left;
+    font-size: calc(100vw / 85);
+  }
+
+  .weather-error {
+    margin-top: calc(100vw / (-102.4));
+  }
+
+  .description-container {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: flex-start;
+    align-items: center;
+    column-gap: calc(100vw / 85.3);
+  }
+
+  .weather-icon {
+    font-size: calc(100vw / 23.27);
+  }
+</style>>
