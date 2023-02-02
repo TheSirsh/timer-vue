e21685a6135a1850b6fdce0fe1c21036cf09c698
TheSirsh <sirsh@bk.ru>
2023-02-02 17:27:14 +0300

    feat: add eng lang file

---
+++ src/assets/json/langEN.js
@@ -0,0 +1,16 @@
+export default {
+  weatherPlaceholder: "Saint Petersburg",
+  weatherError: "The city is not found",
+  months: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "November", "December"],
+  daysName: [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+  greetings: "Good",
+  dayParts: ["Night", "Morning", "Afternoon", "Evening"],
+  greetingsPlaceholder: "Enter your name",
+  todoTitle: "ToDo list",
+  todoPlaceholder: "Enter ToDo",
+  todoButton: "Add",
+  todos: [
+    { id: 1, title: "Not completed", completed: false, },
+    { id: 2, title: "Completed", completed: true, },
+  ], 
+}
